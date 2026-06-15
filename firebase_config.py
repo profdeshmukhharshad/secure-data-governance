682b0a2549cc2e3aa2610a79772b5fc4df1d284c
prof.hdeshmukh <squadvjava@gmail.com>
2026-06-16 00:45:18 +0530

    Fixed Firebase Render deployment

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,36 @@
+import os
 import firebase_admin
 
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import storage
 
+firebase_credentials = {
+
+    "type": "service_account",
+
+    "project_id":
+    os.environ.get(
+        "FIREBASE_PROJECT_ID"
+    ),
+
+    "private_key":
+    os.environ.get(
+        "FIREBASE_PRIVATE_KEY"
+    ).replace("\\n", "\n"),
+
+    "client_email":
+    os.environ.get(
+        "FIREBASE_CLIENT_EMAIL"
+    ),
+
+    "token_uri":
+    "https://oauth2.googleapis.com/token"
+
+}
+
 cred = credentials.Certificate(
-    "serviceAccountKey.json"
+    firebase_credentials
 )
 
 if not firebase_admin._apps:
